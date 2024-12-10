8f3474ceb5ce10b1643561b86b04810ac7959744
Raúl <wychouza@gmail.com>
2024-12-09 16:01:52 -0800

    update benchmarks

--- test/benchmark.ex
+++ test/benchmark.ex
@@ -4,43 +4,39 @@ defmodule Chip.Benchmark.Performance do
   @process :gleam@erlang@process
 
   def run(_scenario) do
-    inputs = %{"10" => 10, "100" => 100, "1000" => 1000, "10000" => 10_000}
+    inputs = %{"10000 records" => 10_000}
 
     Benchee.run(
       %{
-        "chip.members" => fn {registry, id, _group} -> @chip.members(registry, id, 100) end
+        "chip.members" => fn {registry, group} ->
+          @chip.members(registry, group, 100)
+        end
       },
       inputs: inputs,
-      before_scenario: fn set -> before_scenario(1..set) end,
-      before_each: fn {registry, set} -> before_each(registry, set) end,
-      after_scenario: fn {registry, _set} -> after_scenario(registry) end,
+      before_scenario: fn quantity ->
+        {:ok, registry} = @chip.start(:unnamed)
+        initialize_registry(registry, quantity)
+        registry
+      end,
+      before_each: fn registry ->
+        group = Enum.random([:group_a, :group_b, :group_c])
+        {registry, group}
+      end,
+      after_scenario: fn registry ->
+        @chip.stop(registry)
+      end,
       time: 5,
       print: %{configuration: false}
     )
   end
 
-  defp before_scenario(set) do
-    {:ok, registry} = @chip.start(:unnamed)
-
-    for id <- set do
+  defp initialize_registry(registry, records) do
+    for _ <- 1..records do
       group = Enum.random([:group_a, :group_b, :group_c])
       @clock.start(registry, group, 0)
-
-      if Integer.mod(id, 5000) == 0 do
-        :ok = wait_for_clear_message_queue(registry)
-      end
+      :ok = wait_for_clear_message_queue(registry)
     end
 
-    {registry, set}
-  end
-
-  defp before_each(registry, set) do
-    {registry, Enum.random(set), Enum.random([:group_a, :group_b, :group_c])}
-  end
-
-  defp after_scenario(registry) do
-    @chip.stop(registry)
-    IO.puts("")
     nil
   end
 
